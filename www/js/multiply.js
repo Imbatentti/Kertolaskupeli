01b6c84786e719b8677be85c5e245827a2eab9b9
Imbatentti <ru334@hotmail.com>
2015-10-01 20:37:12 +0300

    Cleaning up the code

--- www/js/multiply.js
+++ www/js/multiply.js
@@ -1,56 +1,59 @@
-var rand1= 1 + Math.floor(Math.random() * 10);
-var rand2= 1 + Math.floor(Math.random() * 10);
-var question = (rand1 * rand2);
-var counter =0;
-$("#question").text(rand1 + "*" + rand2);
-$("#counter").text(counter);
+function getQuestion(){
+	rand1= 1 + Math.floor(Math.random() * 10);
+	rand2= 2 + Math.floor(Math.random() * 10);
+	question = (rand1 * rand2);
+	$("#question").text(rand1 + "*" + rand2);
+}
+
+function getPic(){
+	if (counter == 1){
+		$("#matti").attr('src', 'img/matti1.png');
+	}
+	else if (counter == 2){
+		$("#matti").attr('src', 'img/matti2.png');
+	}
+	else if (counter == 3){
+		$("#matti").attr('src', 'img/matti3.png');
+	}
+	else if (counter == 4){
+		$("#matti").attr('src', 'img/matti4.png');
+	}
+	else if (counter == 5){
+		$("#matti").attr('src', 'img/matti5.png');
+		$("#result").text("Matti sai puettua kaiken tarpeellisen, voitit pelin!")
+			
+	}
+}
 
 function checkCorrect(){
 	if (counter < 5){
 		var pAnsw=$("#pAnswer").val();
-		if(pAnsw != null){
-			if (pAnsw == question){
-				$("#result").text("Vastasit oikein!");
-				counter++;
-				
-			}
-			else {
-				$("#result").text("Matti kuoli kylmyyteen, syy on sinun.")
-				counter =0;
-				$("#matti").attr('src', 'img/mattirip.png');
-				end;
-				
-			}
+		if (pAnsw == question){
+			$("#result").text("Vastasit oikein!");
+			counter++;		
 		}
-		$("#counter").text(counter);
-		rand1= 1 + Math.floor(Math.random() * 10);
-		rand2= 2 + Math.floor(Math.random() * 10);
-		question = (rand1 * rand2);
-		$("#question").text(rand1 + "*" + rand2);
-		if (counter == 1){
-			$("#matti").attr('src', 'img/matti1.png');
-		}
-		else if (counter == 2){
-			$("#matti").attr('src', 'img/matti2.png');
-		}
-		else if (counter == 3){
-			$("#matti").attr('src', 'img/matti3.png');
-		}
-		else if (counter == 4){
-			$("#matti").attr('src', 'img/matti4.png');
-		}
-		else if (counter == 5){
-			$("#matti").attr('src', 'img/matti5.png');
-			$("#result").text("Matti sai puettua kaiken tarpeellisen, voitit pelin!")
-			
+		else {
+			$("#result").text("Matti kuoli kylmyyteen, syy on sinun.")
+			counter =0;
+			$("#matti").attr('src', 'img/mattirip.png');
+			end;		
 		}
 		
+		getQuestion();
+		getPic();	
 	}
-	
 	$("#pAnswer").val('');
 }
 
+function onReady(checkCorrect){
+	getQuestion();
+}
 
+//main ja globaalit muuttujat
+var rand1;
+var rand2;
+var question;
+var counter =0;
 $(document).ready(onReady);
 
 
